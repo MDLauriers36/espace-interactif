3c0a0f3d137bc14cccfbcbff2534e578f6eaa61e
MDLauriers36 <90851590+MDLauriers36@users.noreply.github.com>
2022-11-23 16:17:20 -0500

    Bare minimum

--- Assets/script_perso.cs
+++ Assets/script_perso.cs
@@ -1,29 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class script_perso : MonoBehaviour
 {
-    // Start is called before the first frame update
-    public float moveSpeed = 5f;
+    Rigidbody2D body;
+    private SpriteRenderer sp;
 
-    public Rigidbody2D rb;
+    float horizontal;
+    float vertical;
+    float moveLimiter = 0.7f;
+   
+    public float runSpeed = 20.0f;
 
-    Vector2 movement;
     void Start()
     {
-        
+        body = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
+        // Gives a value between -1 and 1
+        horizontal = Input.GetAxisRaw("Horizontal"); // -1 is left
+        vertical = Input.GetAxisRaw("Vertical"); // -1 is down
     }
 
-     void FixedUpdate()
+    void FixedUpdate()
     {
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        if (horizontal != 0 && vertical != 0) // Check for diagonal movement
+        {
+            // limit movement speed diagonally, so you move at 70% speed
+            horizontal *= moveLimiter;
+            vertical *= moveLimiter;
+        }
+
+        body.velocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
+
+
+        if (Input.GetAxisRaw("Horizontal") > 0)
+            {
+            sp.flipX = false;
+            }
+            else if (Input.GetAxisRaw("Horizontal") < 0)
+            {
+            sp.flipX = true;
+            }
+        
+           
     }
+
+
 }
